f1239ae66cc28ab665032702d5aebc8b06b91dbe
Mohammad-Haseeb <mohammad.haseeb310@gmail.com>
2021-02-12 02:03:11 +0500

    Pages link

---
+++ day-one-learning-gatsby/src/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'gatsby';
+
+export  default ()=>{
+   return(
+   <>
+    <h1> Hello it's about page</h1>
+    <Link to="/">index home</Link>
+    
+    </>
+   );
+}
--- day-one-learning-gatsby/src/pages/index.js
+++ day-one-learning-gatsby/src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import {Link} from 'gatsby';
 export default()=>{
   return(
 
      <>
      <h1>Hello World</h1>
+     <Link to="/about">About</Link>
      </>
   );
 }
