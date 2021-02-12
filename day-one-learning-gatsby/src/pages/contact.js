eb9df22488ccedca48f91ce8e28406d1a7eef14c
Mohammad-Haseeb <mohammad.haseeb310@gmail.com>
2021-02-13 01:14:11 +0500

    Update contact.js

--- day-one-learning-gatsby/src/pages/contact.js
+++ day-one-learning-gatsby/src/pages/contact.js
@@ -1,14 +1,14 @@
-// import React from 'react';
-// import Layout from '../components/Layout';
-// function Contact() {
+import React from 'react';
+import Layout from '../components/Layout';
+function Contact() {
 
-//     return (
-//         <Layout>
-//         <div>
-//            <h1>Contact Addrss  Page</h1> 
-//         </div>
-//         </Layout>
-//     )
-// }
+    return (
+        <Layout>
+        <div>
+           <h1>Contact Addrss  Page</h1> 
+        </div>
+        </Layout>
+    )
+}
 
-// export default Contact
+export default Contact
